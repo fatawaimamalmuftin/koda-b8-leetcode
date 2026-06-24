4426509209d0564af4ae26f3fbe63d62882f93d9
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-24 16:04:37 +0700

    feat: TwoSun leetCode

--- JumlahDua.js
+++ JumlahDua.js
@@ -26,5 +26,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target){
+                return console.log([i, j])
+            }            
+        }        
+    }    
 };
+
+twoSum([3,3], 6)// output : [0,1]
+console.log("------------------------------")
+twoSum([2,7,11,15], 9)// output : [0,1]
+console.log("------------------------------")
+twoSum([3,2,4], 6)// output : [1,2]
