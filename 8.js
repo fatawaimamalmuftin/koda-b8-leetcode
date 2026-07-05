db359def3b62647291adeb2c8c16670b3b97f6c2
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-07-05 18:42:39 +0700

    feat: function reverse int until 32 bit

---
+++ 8.js
@@ -0,0 +1,67 @@
+/**
+ * Reverse Integer
+ * 
+ * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ * 
+ * Example 1:
+ * Input: x = 123
+ * Output: 321
+ * 
+ * Example 2:
+ * Input: x = -123
+ * Output: -321
+ * 
+ * Example 3:
+ * Input: x = 120
+ * Output: 21
+ * 
+ * Constraints:
+ * -231 <= x <= 231 - 1
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+
+    // Cek apakah angka negatif
+    let negatif = false;
+
+    if (x < 0) {
+        negatif = true;
+        x = Math.abs(x);
+    }
+
+    // Balik angka
+    let hasil = Number(
+        String(x)
+            .split("")
+            .reverse()
+            .join("")
+    );
+
+    // Kembalikan tanda minus
+    if (negatif) {
+        hasil = -hasil;
+    }
+
+    // Batas integer 32-bit
+    let batasBawah = -(2 ** 31);
+    let batasAtas = (2 ** 31) - 1;
+
+    if (hasil < batasBawah) {
+        return 0;
+    }
+
+    if (hasil > batasAtas) {
+        return 0;
+    }
+
+    console.log(hasil);
+    // return hasil;
+};
+
+reverse(123)
+reverse(-123)
+reverse(120)
